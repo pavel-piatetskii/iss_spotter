0d2ac27d26b8e113e90741ac44c94245f1722fe4
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-06 01:42:20 +0000

    Completed ISS Spotter II

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // External modules and aliases
 const { log } = console;
-const { fetchMyIP } = require('./iss');
-
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // ---- Get IP-address ------
-let ip = '';
-fetchMyIP((err, data) => {
-  if (err) log(err); // Error
-  ip = data;         // Set IP
-});
+// let ip = '';
+// fetchMyIP( (err, data) => {
+//   if (err) log(err); // Error
+//   ip = data;         // Set IP
+// });
 
+// let coords = {}
+// fetchCoordsByIP(ip, (err, data) => {
+//   if (err) log(err); // Error
+//   coords = data;
+// });
--- iss.js
+++ iss.js
@@ -1,6 +1,7 @@
 // External modules and aliases
 const request = require('request');
 
+// ------- Get IP-address via API ---------
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org/?format=json', (error, response, body) => {
     if (error) callback(error, null);
@@ -15,4 +16,33 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+// --------- Get coordinates via API ----------
+const fetchCoordsByIP = function(ip, callback) {
+  request('http://ip-api.com/json/' + ip, (error, response, body) => {
+
+    // If request returns error
+    if (error) callback(error, null);
+
+    // if response is not code 200
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      callback(Error(msg), null);
+    }
+
+    // Parse JSON string into object (we need this due to the API especialities)
+    const reply = JSON.parse(body);
+
+    // If API replied with 'fail' status - throw an Error
+    if (reply.status === 'fail') {
+      const msg = `Request failed! ${body}`;
+      callback(Error(msg), null);
+    }
+
+    // Success
+    const { lat, lon } = reply;
+    callback(null, { lon, lat });
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
